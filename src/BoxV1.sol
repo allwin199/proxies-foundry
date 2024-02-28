c85b69b645be29def651f0291d3910adb53d8b9f
worksofallwin <princeallwin199@gmail.com>
2024-02-28 19:47:00 +0530

    complete boxv1 and boxv2

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -9,17 +9,28 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/contracts/pro
 /// @notice storage is stored in the proxy, NOT the implementation
 /// @notice proxy will borrow the implementation functions
 /// @notice proxy should deploy the implementation contract and then it should call some "initializer" function.
+
+// Note: Storage is stored in proxy, Not in the implementation
+// "When using proxies with upgradeable contracts, any changes made within the implementation contract's constructor wouldn't directly update the existing state of the deployed proxy contract".
+// Therfore, implementation contract shouldn't have anything inside the constructor
+
 contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+        // _disableInitializers will put here to say don't let any initialization happen here
     }
 
+    // this `initialize` function will act as `constructor` for `proxy` contracts
+    // when this contract is deployed
+    // proxy contract will immediately call this `initialize` function
+    // which will make `proxy` contract to be owner of this contract
     function initialize(address initialOwner) public initializer {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
+        // this fn dosen't change anything but since it is `UUPSUpgradeable` we have placed it here
     }
 
     function getNumber() external view returns (uint256) {
